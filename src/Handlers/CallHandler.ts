19b6b314cc3c355fbd05a34469bbae5e18d00af0
Fantox <shubhapratimbiswas@gmail.com>
2022-04-23 21:00:14 +0530

    Update CallHandler.ts

--- src/Handlers/CallHandler.ts
+++ src/Handlers/CallHandler.ts
@@ -33,7 +33,7 @@ export default class CallHandler {
         await this.client.sendWA(`${tag},${JSON.stringify(json)}`)
         await this.client.sendMessage(
 					caller,
-					`You'll be banned for calling the bot, Baka!`,
+					`Didn't you read the warning not to call me, Baka! You will get blocked automatically for calling me.`,
 					MessageType.text
 				);
 				await this.client.blockUser(caller);
