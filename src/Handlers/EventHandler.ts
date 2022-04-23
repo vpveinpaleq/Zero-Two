72767f6a2e04cd4fcfbd10126967e8dde99a7f92
Fantox <shubhapratimbiswas@gmail.com>
2022-04-23 22:07:22 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -19,7 +19,7 @@ export default class EventHandler {
         const text = add
 					? `- ${group.subject || "___"} -\n\n💠 *Group Description:*\n${
 							group.desc
-					  }\n\nHope you follow all the rules and have fun Darling!\n\n${event.participants
+					  }\n\nHope you will follow all the rules and have fun Darling!\n\n${event.participants
 							.map((jid) => `@${jid.split("@")[0]}`)
 							.join(", ")}`
 					: event.action === "remove"
