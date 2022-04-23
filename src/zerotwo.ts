b62b8a7918e204ccf3829f77a50d224fbcf59873
Fantox <shubhapratimbiswas@gmail.com>
2022-04-23 22:16:36 +0530

    Update zerotwo.ts

--- src/zerotwo.ts
+++ src/zerotwo.ts
@@ -15,8 +15,8 @@ import NewsHandler from "./Handlers/NewsHandler";
 
 if (!process.env.MONGO_URI) throw new Error("MONGO URL IS NOT PROVIDED");
 const client = new WAClient({
-  name: process.env.NAME || "Chitoge",
-  session: process.env.SESSION || "Chitoge",
+  name: process.env.NAME || "Zero Two",
+  session: process.env.SESSION || "Zero Two",
   prefix: process.env.PREFIX || ":",
   mods: (process.env.MODS || "")
     .split(", ")
