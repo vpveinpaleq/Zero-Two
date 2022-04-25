4d304fef8a17e6bb51a2d946f87d326495b21dc4
Fantox <shubhapratimbiswas@gmail.com>
2022-04-25 15:37:58 +0530

    Update play2.ts

--- src/commands/Media/play2.ts
+++ src/commands/Media/play2.ts
@@ -25,7 +25,7 @@ export default class Command extends BaseCommand {
         if (!videos || videos.length <= 0) return void M.reply(`⚓ No Matching videos found for the term : *${term}*`)
         const video = new YT(videos[0].url, 'video')
         if (!video.url) return
-        M.reply('⚡ Sending...')
+        M.reply('📍 Downloading and Sending...')
         this.client
             .sendMessage(M.from, await video.getBuffer(), MessageType.video, {
                 quoted: M.WAMessage,
