dc34c440c6dfdfe12c428932cccff1459a6e0da8
Fantox <shubhapratimbiswas@gmail.com>
2022-04-25 13:39:12 +0530

    Update Rules.ts

--- src/commands/General/Rules.ts
+++ src/commands/General/Rules.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://c.tenor.com/sPSsSfnHnqYAAAPo/moon-regras.mp4'
+            'https://c.tenor.com/geMdtLCXZkAAAAPo/rules.mp4'
         ]
         let chitoge = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
