b8174bb47d46f044470018cde83eb5c880a141ee
Fantox <shubhapratimbiswas@gmail.com>
2022-04-25 20:21:09 +0530

    Update BDSM.ts

--- src/commands/Nsfw/BDSM.ts
+++ src/commands/Nsfw/BDSM.ts
@@ -23,7 +23,7 @@ export default class Command extends BaseCommand {
         const { data } = await axios.get('https://ravindumanoj-sew-api.herokuapp.com/main/nsfw/bdsm?apikey=RavinduManoj')
         if ( !(await this.client.getGroupData(M.from)).nsfw)
             return void M.reply(
-                `Don't be a pervert, Baka! This is not an NSFW group.`
+                `Don't be a pervert, Baka! This command is only useable in NSFW groups.`
             )
         const buffer = await request.buffer(data.result).catch((e) => {
             return void M.reply(e.message)
