9a3e45d3965d667f2703885b05881434c70d0dde
Fantox <shubhapratimbiswas@gmail.com>
2022-04-25 20:07:12 +0530

    Update Promote.ts

--- src/commands/Moderation/Promote.ts
+++ src/commands/Moderation/Promote.ts
@@ -18,7 +18,7 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
 					return void M.reply(
-						`How can I promote someone without being an admin?`
+						`📍 Darling tell me, how can I Promote someone without being an Admin?`
 					);
 				if (M.quoted?.sender) M.mentioned.push(M.quoted.sender);
 				if (!M.mentioned.length)
