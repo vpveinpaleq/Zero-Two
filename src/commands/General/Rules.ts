710b65e7906cf480495673989a3b04f1df8ceb78
Fantox <shubhapratimbiswas@gmail.com>
2022-04-25 13:59:56 +0530

    Update Rules.ts

--- src/commands/General/Rules.ts
+++ src/commands/General/Rules.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
         let chitoge = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
             mimetype: Mimetype.gif,
-            caption: `_*----🎀[Rule]🎀----*_\n\n❌*DONT ASK FOR THE SCRIPT*🚫\n*>>>* use $support to get the Official group link in your dm\n*--->*  If you want to chat with Star you can use *Don't chat in private bot can't chat in Pm\n*--->* If you want to add Yotsuba in your group the contact the owner by *$owner/$mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫  ` }
+            caption: `_*----📍[Rules for Zero Two's usage]📍----*_\n\n*>>>* use -support to get the Official group link in your dm\n*--->*  If you want to chat with Zero Two you can use -chat *Don't chat in private bot can't chat in Pm\n*--->* If you want to add Zero Two in your group the contact the owner by *-owner/-mods* \n*--->* Dont use wrong command, use the command given in the *-help* list \n*--->* Dont spam the bot with commands if Zero Two is not responding, its means the maybe owner is offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BANNED* 🚫  ` }
         )
     }
 }
