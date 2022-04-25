46bc8c00b3e78438a9ce3bd29c6e1d7e9272daa7
Fantox <shubhapratimbiswas@gmail.com>
2022-04-25 20:43:55 +0530

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -80,8 +80,8 @@ export default class Command extends BaseCommand {
 					})();
 					return {
 						categories,
-						pack: pack[1] || "⚡ Here you go ",
-						author: pack[2] || "Yotsuba ⚡",
+						pack: pack[1] || "📍 Zero Two Bot📍",
+						author: pack[2] || "By: Fantox",
 						quality,
 						type: StickerTypes[
 							parsedArgs.flags.includes("--crop") ||
