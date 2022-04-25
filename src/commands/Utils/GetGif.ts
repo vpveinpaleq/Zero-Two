a5bc1cc3add4463ce84c483b7abacf3bbe10725c
Fantox <shubhapratimbiswas@gmail.com>
2022-04-25 20:26:52 +0530

    Update GetGif.ts

--- src/commands/Utils/GetGif.ts
+++ src/commands/Utils/GetGif.ts
@@ -26,7 +26,7 @@ export default class Command extends BaseCommand {
 	): Promise<void> => {
 		if (!this.client.config.gifApi)
 			return void M.reply("No key set for searching gifs. ");
-		if (!joined) return void (await M.reply(`GIve me a search term, Baka!`));
+		if (!joined) return void (await M.reply(`Give me a search term, Baka Darling!`));
 		const search = joined.trim();
 		const gif = await axios
 			.get(
@@ -36,7 +36,7 @@ export default class Command extends BaseCommand {
 		if (!gif)
 			return void (await M.reply(`Couldn't find any matching gif term.`));
 		const i = Math.floor(Math.random() * gif.data.results.length);
-		const caption = "🌟 Here you go.";
+		const caption = "📍 Here you go.";
 		return void M.reply(
 			await request.buffer(gif.data.results[i].media[0].mp4.url),
 			MessageType.video,
