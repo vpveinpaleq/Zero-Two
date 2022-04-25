06dad8e0bb7341ee598c5e4dff59c5b73f6da8c1
Fantox <shubhapratimbiswas@gmail.com>
2022-04-25 20:05:39 +0530

    Update Remove.ts

--- src/commands/Moderation/Remove.ts
+++ src/commands/Moderation/Remove.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         let text = '*Action*\n\n'
         if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-            return void M.reply(`How can I remove someone without being an admin?`)
+            return void M.reply(`📍 Darling tell me, how can I Remove someone without being an Admin?`)
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         if (!M.mentioned.length) return void M.reply(`Tag the users you want to ${this.config.command}`)
         M.mentioned.forEach(async (user) => {
@@ -30,7 +30,7 @@ export default class Command extends BaseCommand {
             }
             // check if user is Bot
             else if (this.client.user.jid === user) {
-                text += `✖ Skipped *@${user.split('@')[0]}* as they're me.\n`
+                text += `✖ Skipped *@${user.split('@')[0]}* as that's me. Why do you want to remove me?\n`
             } else {
                 text += `🟥 Removed *@${user.split('@')[0]}*\n`
                 await this.client.groupRemove(M.from, [user])
