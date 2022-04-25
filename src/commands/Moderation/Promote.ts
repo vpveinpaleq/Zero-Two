0c01fce6f2c069558aba1ab38e7ced51edb2f49e
Fantox <shubhapratimbiswas@gmail.com>
2022-04-25 20:07:59 +0530

    Update Promote.ts

--- src/commands/Moderation/Promote.ts
+++ src/commands/Moderation/Promote.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
         M.mentioned.forEach(async (user) => {
             const usr = this.client.contacts[user]
             const username = usr.notify || usr.vname || usr.name || user.split('@')[0]
-            if (M.groupMetadata?.admins?.includes(user)) M.reply(`✖ Skipped *${username}* as they're already an admin`)
+            if (M.groupMetadata?.admins?.includes(user)) M.reply(`✖ Skipped *${username}* as they're also an admin`)
             else {
                 await this.client.groupMakeAdmin(M.from, [user])
                 M.reply(`👑 Successfully Promoted *${username}*`)
