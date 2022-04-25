e3f2c43e6b2bfebd9e5c95b3297b055f9c2dbc9f
Fantox <shubhapratimbiswas@gmail.com>
2022-04-25 20:20:32 +0530

    Update Anal.ts

--- src/commands/Nsfw/Anal.ts
+++ src/commands/Nsfw/Anal.ts
@@ -23,7 +23,7 @@ export default class Command extends BaseCommand {
 		caption += `📍`;
 		if (!(await this.client.getGroupData(M.from)).nsfw)
 			return void M.reply(
-				`Don't be a pervert, Baka! This is not an NSFW group.`
+				`Don't be a pervert, Baka! This command is only useable in NSFW groups.`
 			);
 		M.reply(
 			await this.client.util.GIFBufferToVideoBuffer(
