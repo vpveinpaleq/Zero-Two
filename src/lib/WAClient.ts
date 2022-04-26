d21fce66c2f5f2dfbda5d571e3f82c658de104b0
Fantox <shubhapratimbiswas@gmail.com>
2022-04-26 16:09:54 +0530

    Update WAClient.ts

--- src/lib/WAClient.ts
+++ src/lib/WAClient.ts
@@ -167,7 +167,7 @@ export default class WAClient extends Base {
 
 	log = (text: string, error?: boolean): void => {
 		console.log(
-			chalk[error ? "red" : "green"]("[ZeroTwo]"),
+			chalk[error ? "red" : "green"]("[CHITOGE]"),
 			chalk.blue(moment(Date.now() * 1000).format("DD/MM HH:mm:ss")),
 			chalk.yellowBright(text)
 		);
