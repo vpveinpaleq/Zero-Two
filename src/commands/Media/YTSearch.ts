1d9dec29a916f4d8658e0387a3d93503003f557c
Fantox <shubhapratimbiswas@gmail.com>
2022-04-26 21:02:05 +0530

    Update YTSearch.ts

--- src/commands/Media/YTSearch.ts
+++ src/commands/Media/YTSearch.ts
@@ -29,14 +29,14 @@ export default class Command extends BaseCommand {
                 videos[i].author.name
             }\n 📙 *Duration:* ${videos[i].duration}\n📘 *URL:* ${videos[i].url}\n\n`
         }
-        M.reply('⚡ Searching...')
+        M.reply('📍 Searching for you...')
         this.client
             .sendMessage(M.from, text, MessageType.extendedText, {
                 quoted: M.WAMessage,
                 contextInfo: {
                     externalAdReply: {
                         title: `Search Term: ${term}`,
-                        body: `⚡ Yotsuba ⚡`,
+                        body: `📍 Zero Two 📍`,
                         mediaType: 2,
                         thumbnailUrl: videos[0].thumbnail,
                         mediaUrl: videos[0].url
