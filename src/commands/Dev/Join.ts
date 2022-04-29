ae17498ab226f2d9c794810c1b6463abb7228984
Fantox <shubhapratimbiswas@gmail.com>
2022-04-29 12:59:13 +0530

    Update Join.ts

--- src/commands/Dev/Join.ts
+++ src/commands/Dev/Join.ts
@@ -16,10 +16,10 @@ export default class Command extends BaseCommand {
         })
     }
 
-    run = async (M: ISimplifiedMessage): Promise<void> => {
+   run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!M.urls.length) return void M.reply('Link?')
         const url = M.urls.find((url) => url.includes('chat.whatsapp.com'))
-        if (!url) return void M.reply('No WhatsApp Invite URLs found in your message')
+        if (!url) return void M.reply('Ara ara, no WhatsApp Invite URLs found in your message')
         if (this.client.config.mods?.includes(M.sender.jid)) {
             const groups = this.client.chats
                 .all()
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
                 .map((chat) => chat.jid)
             const s = url.split('/')
             const { status, gid } = await this.client.acceptInvite(s[s.length - 1]).catch(() => ({ status: 401 }))
-            if (status === 401) return void M.reply('Darling i cannot join this group. Maybe, because i was removed from there before')
+            if (status === 401) return void M.reply('Cannot join group. Maybe, I was removed from there before')
             if (groups.includes(gid)) return void M.reply('Already there')
             return void M.reply(`Joined ${(await this.client.fetchGroupMetadataFromWA(gid)).subject}`)
         }
