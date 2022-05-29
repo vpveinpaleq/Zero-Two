a084e8afab450963f0bff87e2f58efb2000f4595
Fantox <shubhapratimbiswas@gmail.com>
2022-05-29 21:58:34 +0530

    Update InviteLink.ts

--- src/commands/General/InviteLink.ts
+++ src/commands/General/InviteLink.ts
@@ -12,7 +12,7 @@ export default class Command extends BaseCommand {
             description: 'Get the group invite link',
             category: 'general',
             usage: `${client.config.prefix}invite`,
-            baseXp: 10
+            baseXp: 300
         })
     }
 
