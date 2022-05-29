15384a6571e84ec31ac52fc01a411f90871f8d10
Fantox <shubhapratimbiswas@gmail.com>
2022-05-29 21:44:16 +0530

    Update Wanted.ts

--- src/commands/Fun/Wanted.ts
+++ src/commands/Fun/Wanted.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 			description: "Well... make someone wanted",
 			category: "fun",
 			usage: `${client.config.prefix}wanted [tag/quote]`,
-			baseXp: 30,
+			baseXp: 200,
 		});
 	}
 
