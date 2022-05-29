a04cb422c6897a86cdbae3f17d5c1b3353ed3bea
Fantox <shubhapratimbiswas@gmail.com>
2022-05-29 21:42:52 +0530

    Update RIP.ts

--- src/commands/Fun/RIP.ts
+++ src/commands/Fun/RIP.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 			description: "RIP",
 			category: "fun",
 			usage: `${client.config.prefix}rip [tag/quote]`,
-			baseXp: 30,
+			baseXp: 200,
 		});
 	}
 
