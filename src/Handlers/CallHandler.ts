2e027319c60f623e715842582284da86af0c92db
Vpveinpaleq <108933179+vpveinpaleq@users.noreply.github.com>
2022-07-29 13:03:39 +0300

    Update CallHandler.ts

--- src/Handlers/CallHandler.ts
+++ src/Handlers/CallHandler.ts
@@ -33,10 +33,9 @@ export default class CallHandler {
         await this.client.sendWA(`${tag},${JSON.stringify(json)}`)
         await this.client.sendMessage(
 					caller,
-					`Didn't you read the warning not to call me, now i am going to *block* and *ban* you for calling me. \nYour every call makes the bot *crash!*`,
+					` `,
 					MessageType.text
 				);
-				await this.client.blockUser(caller);
-				await this.client.banUser(caller);
+				
     }
 }
